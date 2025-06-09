a4b650140e981c050e38f95c367599edf81abc02
KamSaf <bopaf42@gmail.com>
2025-06-09 12:24:04 +0200

    Create prune function (non functional though as it seems)

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
-from tree import Tree
+from node import Node
 
 if __name__ == "__main__":
     # Text variant
-    tree = Tree.visualise(data_path="../data/gielda.txt")
+    tree = Node.visualise(data_path="../data/gielda.txt")
     print(tree)
     # save_tree(tree)
 
     # Structure variant
-    root = Tree.build_tree_struct(data_path="../data/gielda.txt")
+    root = Node.build_tree_struct(data_path="../data/car.data")
+    if root:
+        root.prune()
     print(root)
     # save_tree(str(root))
--- src/node.py
+++ src/node.py
@@ -1,5 +1,33 @@
+from math import inf
 from uuid import uuid1, UUID
-from config import INDENT
+from config import DECISION_COLUMN_SYMBOL, DATA_FILE_PATH, INDENT
+from utils import (
+    read_data,
+    get_max_ratio_attr,
+    split_dict,
+    get_unique_values,
+)
+
+
+def get_max_key(vals_dict: dict[str | None, int]) -> str | None:
+    """
+    Function finding key with max value in dictionary.
+
+    Params:
+        vals_dict (dict[str | None, int]): dictionary to be searched
+
+    Returns:
+        max_key (str | None): key with maximum value, None if no definite winner
+    """
+    max_val = -inf
+    max_key = ""
+    for key, val in vals_dict.items():
+        if val == max_val:
+            return None
+        if val > max_val:
+            max_key = key
+            max_val = val
+    return max_key
 
 
 class Node:
@@ -16,7 +44,7 @@ class Node:
 
     def __init__(
         self,
-        label: str,
+        label: str = "node",
         children: list["Node"] | None = None,
         val: str | None = None,
         parent_id: UUID | None = None,
@@ -117,16 +145,134 @@ class Node:
     def __str__(self) -> str:
         return self.to_string()
 
+    @staticmethod
+    def visualise(
+        data: dict | None = None,
+        data_path: str = DATA_FILE_PATH,
+        level: int = 0,
+        output: list = [],
+    ) -> str | None:
+        """
+        Function building text decision tree visualisation.
+
+        Parameters:
+            data: (dict | None): data in form of a dictionary (key - attribute, value - column content)
+
+            data_path (str): path to dataset file
+
+            level (int): level of indentation
+
+            output (list): list containing output of visualisation
+
+        Returns:
+            tree (str | None): tree visualisation in text form
+        """
+        if not data:
+            data = read_data(data_path)
+        attr, ratio = get_max_ratio_attr(data)
+        if abs(ratio) == 0:
+            output.append(f"D: {tuple(set(data[DECISION_COLUMN_SYMBOL]))[0]}")
+            return
+        output.append(f"Atrybut: {attr[1]}")
+        level += 1
+        split_data = split_dict(data, get_unique_values(data)[attr], attr)
+        for sd in split_data.values():
+            if level != 0:
+                output.append(f"\n{level*INDENT}{sd[attr][0]} -> ")
+            Node.visualise(sd, level=level, output=output)
+        return "".join(output)
+
+    @staticmethod
+    def build_tree_struct(
+        root: "Node | None" = None,
+        data: dict | None = None,
+        data_path: str = DATA_FILE_PATH,
+    ) -> "Node | None":
+        """
+        Function building decision tree structure.
+
+        Parameters:
+            root: (Node | None): root from which tree will be built
+
+            data_path (str): path to dataset file
+
+            data_path (str): path to dataset file
+        Returns:
+            tree (Node | None): decision tree
+        """
+        if root is None:
+            root = Node()
+        if root.label and "DECISION" in root.label:
+            return
+        if not data:
+            data = read_data(data_path)
+        attr, ratio = get_max_ratio_attr(data)
+        if abs(ratio) == 0:
+            root.append_child(
+                Node(f"DECISION: {tuple(set(data[DECISION_COLUMN_SYMBOL]))[0]}")
+            )
+            return
+        root.label = attr
+        split_data = split_dict(data, get_unique_values(data)[attr], attr)
+        for sd in split_data.values():
+            decision_column_values = tuple(
+                get_unique_values(sd)[DECISION_COLUMN_SYMBOL]
+            )
+            label = (
+                f"DECISION: {decision_column_values[0]}"
+                if len(decision_column_values) == 1
+                else "node"
+            )
+            new_node = Node(label=label, val=f"{sd[attr][0]}", parent_id=root.id)
+            root.append_child(new_node)
+            Node.build_tree_struct(new_node, sd)
+        return root
+
+    def prune(self) -> None | str:
+        """
+        Recursive method pruning decision tree.
+
+        Returns:
+            node_label (str): only in recursive calls
+        """
+        if not self.children:
+            return self.label
+        children_labels = list(
+            filter(lambda lab: lab is not None, [c.prune() for c in self.children])
+        )
+        labels = {lab: children_labels.count(lab) for lab in set(children_labels)}
+        max_key = get_max_key(labels)
+        if max_key:
+            self.label = max_key
+            self.children.clear()
+
+    def predict(self):
+        pass
+
+    def train_and_test(self):
+        pass
+
+    def cross_val(self):
+        pass
+
+    def evaluate(self):
+        pass
+
 
 if __name__ == "__main__":
     tree = Node(
-        label="root",
-        val="0",
+        label="attr1",
         children=[
-            Node(label="leaf", val="1"),
-            Node(label="node", val="2", children=[Node(label="leaf2", val="3")]),
+            Node(label="DECISION: B", val="0"),
+            Node(
+                label="attr2",
+                val="1",
+                children=[
+                    Node(label="DECISION: A", val="no"),
+                    Node(label="DECISION: A", val="yes"),
+                ],
+            ),
         ],
     )
-
+    tree.prune()
     print(tree)
-    print(tree.get_depth())
